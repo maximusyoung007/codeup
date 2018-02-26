d812ca4e15b89cbb4e1a05fbbef16bb916774305
maximusyoung007 <1091261998@qq.com>
2018-02-26 11:35:23 +0800

    递归

--- 递归/数列.cpp
+++ 递归/数列.cpp
@@ -1,14 +1,12 @@
-#include <iostream>
 #include<cstdio>
-using namespace std;
 int fun(int n)
 {
-    if(n==0)
+    if(n == 0)
         return 0;
-    else if(n==1)
+    else if(n == 1)
         return 1;
     else
-        return fun(n-1)+fun(n-2);
+        return fun(n - 1) + fun(n - 2);
 }
 int main()
 {
@@ -16,14 +14,14 @@ int main()
     int m;
     scanf("%d",&m);
         while(m--){
-                scanf("%d",&n);
-             if(n==0)
-            break;
-        for(int i=0;i<n;++i)
+             scanf("%d",&n);
+             if(n == 0)
+                break;
+        for(int i = 0;i < n;++i)
         {
-            for(int j=0;j<(2*n-1)-(2*i+1);++j)
+            for(int j = 0;j < (2 * n - 1)-(2 * i + 1);++j)
                 printf(" ");
-            for(int k=0;k<2*i+1;++k)
+            for(int k = 0;k < 2 * i + 1;++k)
                 printf("%d ",fun(k));
             printf("\n");
         }
