0b21d13db5e28560f99337792c41c1e8f000d11a
maximusyoung007 <1091261998@qq.com>
2018-06-23 20:13:22 +0800

    vector

--- STL-vector/student list for course.cpp
+++ STL-vector/student list for course.cpp
@@ -1,33 +1,43 @@
+#include<iostream>
 #include<cstdio>
-#include<cstring>
 #include<vector>
 #include<algorithm>
+#include<string.h>
 using namespace std;
-const int maxn = 40010;
-const int maxc = 2510;
-char name[maxn][5];
-vector<int> course[maxc];
+const int maxn = 26 * 26 * 26 * 10;
+vector<int> vi[maxn];
+char stu_name[3000][5];
+/**int getId(char c[])
+{
+    int id = 0;
+    for(int i = 0;i < 3;i++){
+        id = id * 26 + (c[i] - 'A');
+    }
+    id = id * 10 + (c[3] - '0');
+}**/
 bool cmp(int a,int b)
 {
-    return strcmp(name[a],name[b]) < 0;
+    return strcmp(stu_name[a],stu_name[b]) < 0;
 }
 int main()
 {
-    int n,k,c,courseid;
-    scanf("%d%d",&n,&k);
-    for(int i = 0;i < n;i++){
-        scanf("%s%d",name[i],&c);
-        for(int j = 0;j < c;j++){
-            scanf("%d",&courseid);
-            course[courseid].push_back(i);
+    int stu_num,course_num;
+    scanf("%d %d\n",&stu_num,&course_num);
+    int stu_id,course_id;
+    int stu_course;
+    for(int i = 0;i < stu_num;i++){
+        scanf("%s %d",stu_name[i],&stu_course);
+        for(int j = 0;j < stu_course;j++){
+            scanf("%d",&course_id);
+            //stu_id = getId(stu_name);
+            vi[course_id].push_back(i);
         }
     }
-    for(int i = 1;i <= k;i++){
-        printf("%d %d\n",i,course[i].size());
-        sort(course[i].begin(),course[i].end(),cmp);
-        for(int j = 0;j < course[i].size();j++){
-            printf("%s\n",name[course[i][j]]);
+    for(int i = 1;i <= course_num;i++){
+        printf("%d %d",i,vi[i].size());
+        sort(vi[i].begin(),vi[i].end(),cmp);
+        for(int j = 0;j < vi[i].size();j++){
+            printf("%s\n",stu_name[vi[i][j]]);
         }
     }
-    return 0;
 }
