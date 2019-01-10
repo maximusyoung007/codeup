1066af8c7c86eb72c1ead993509c3279b7d927e5
maximusyoung007 <1091261998@qq.com>
2019-01-10 16:27:00 +0800

    。

--- 2.3选择结构/25334.c
+++ 2.3选择结构/25334.c
@@ -11,8 +11,8 @@ int main()
 	double a,b,c,t;
 	scanf("%lf %lf %lf",&a,&b,&c);
 	if(a>b)swap(&a,&b);
-    if(a>c)swap(&a,&c);
-    if(b>c)swap(&b,&c);
-    printf("%0.2f %0.2lf %0.2lf\n",a,b,c);
-    return 0;
+    	if(a>c)swap(&a,&c);
+    	if(b>c)swap(&b,&c);
+    	printf("%0.2f %0.2lf %0.2lf\n",a,b,c);
+    	return 0;
 }
