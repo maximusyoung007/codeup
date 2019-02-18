3350e5319000d9dd58f8f105e908e75c535ee9c6
maximusyoung007 <1091261998@qq.com>
2019-02-18 14:20:36 +0800

    。

--- 3.3图形输出/2506.cpp
+++ 3.3图形输出/2506.cpp
@@ -2,19 +2,21 @@
 int main()
 {
     int n;
-    scanf("%d",&n);
-    for(int i = 0;i < n - 1;i++){
-        for(int j = 0;j < i;j++)
-            printf(" ");
-        for(int j = 0;j < n - i;j++)
-            printf("* ");
-        printf("\n");
+    while(scanf("%d",&n) != EOF){
+        for(int i = 0;i < n - 1;i++){
+            for(int j = 0;j < i;j++)
+                printf(" ");
+            for(int j = 0;j < n - i;j++)
+                printf("* ");
+            printf("\n");
+        }
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < n - 1 - i;j++)
+                printf(" ");
+            for(int j = 0;j < i + 1;j++)
+                printf("* ");
+            printf("\n");
+        }
     }
-    for(int i = 0;i < n;i++){
-        for(int j = 0;j < n - 1 - i;j++)
-            printf(" ");
-        for(int j = 0;j < i + 1;j++)
-            printf("* ");
-        printf("\n");
-    }
+    return 0;
 }
