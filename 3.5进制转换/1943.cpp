e727005e632ac5da22300d4d9bdef6e4611dca68
maximusyoung007 <1091261998@qq.com>
2019-03-18 12:21:44 +0800

    进制转换

--- 3.5进制转换/1943.cpp
+++ 3.5进制转换/1943.cpp
@@ -1,7 +1,6 @@
 #include<cstdio>
 #include<string.h>
 char result[1000];
-//int k = 0;
 void divide_two(char n[])
 {
     int j = -1,temp = 0;
@@ -9,12 +8,17 @@ void divide_two(char n[])
     while(test){
         test = 0;
         for(int i = 0;i < strlen(n);i++){
+            //计算余数
             temp = (n[i] - '0') / 2;
+            //最终除数的每一位都应该是零
             test += temp;
+            //如果是最后一位，就把这一位运算的余数装进结果数组中
             if(i == strlen(n) - 1)
                 result[++j] = n[i] % 2 + '0';
+            //如果不是最后一位，计算下一位被除数
             else
                 n[i + 1] += (n[i] - '0') % 2 * 10;
+            //计算下一次循环的除数
             n[i] = temp + '0';
         }
     }
