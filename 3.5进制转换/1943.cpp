46ae9157a665d1d534879bc6dbe1c0473ffcfbcb
maximusyoung007 <1091261998@qq.com>
2019-03-18 12:25:27 +0800

    进制转换

--- 3.5进制转换/1943.cpp
+++ 3.5进制转换/1943.cpp
@@ -8,9 +8,9 @@ void divide_two(char n[])
     while(test){
         test = 0;
         for(int i = 0;i < strlen(n);i++){
-            //计算余数
+            //计算商
             temp = (n[i] - '0') / 2;
-            //最终除数的每一位都应该是零
+            //被除数每一位都是0时，停止while循环
             test += temp;
             //如果是最后一位，就把这一位运算的余数装进结果数组中
             if(i == strlen(n) - 1)
@@ -18,7 +18,7 @@ void divide_two(char n[])
             //如果不是最后一位，计算下一位被除数
             else
                 n[i + 1] += (n[i] - '0') % 2 * 10;
-            //计算下一次循环的除数
+            //计算下一次for循环的除数
             n[i] = temp + '0';
         }
     }
