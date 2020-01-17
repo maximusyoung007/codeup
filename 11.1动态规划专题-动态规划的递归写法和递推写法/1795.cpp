c2f84e4a26f81ec95992653cf2c5ccc9f0980b2f
maximus <1091261998@qq.com>
2020-01-17 16:54:12 +0800

    动态规划11.1

---
+++ 11.1动态规划专题-动态规划的递归写法和递推写法/1795.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int f(int n)
+{
+	if (n == 0)
+		return 0;
+	else if (n == 1)
+		return 1;
+	int f0 = 0, f1 = 1;
+	int result = 0;
+	for (int i = 2; i <= n; i++)
+	{
+		result = f0 + f1;
+		f0 = f1;
+		f1 = result;
+	}
+	return result;
+}
+int main()
+{
+	int n;
+	while (cin >> n)
+		cout << f(n) << endl;
+}
