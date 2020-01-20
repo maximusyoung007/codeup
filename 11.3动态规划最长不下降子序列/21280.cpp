d0f13221bb5ec4ed25283a533086928844935227
maximus <1091261998@qq.com>
2020-01-20 17:16:55 +0800

    LIS

---
+++ 11.3动态规划最长不下降子序列/21280.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include<algorithm>
+using namespace std;
+const int maxn = 1100;
+int a[maxn], dp[maxn];
+int main()
+{
+    int n;
+    while (cin >> n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> a[i];
+        }
+        int ans = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            dp[i] = 1;
+            for (int j = 1; j < i; j++)
+            {
+                if (a[i] >= a[j] && dp[j] + 1 > dp[i])
+                    dp[i] = dp[j] + 1;
+            }
+            ans = max(ans, dp[i]);
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
