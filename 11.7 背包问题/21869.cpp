974f0e12fba026a680a024efe7746d5d2e78b0ac
maximus <1091261998@qq.com>
2020-01-28 15:31:45 +0800

    背包问题

--- 11.7 背包问题/21869.cpp
+++ 11.7 背包问题/21869.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-const int maxn = 20010;
-const int maxv = 20010;
+const int maxn = 10010;
+const int maxv = 10010;
 int w[maxn];
 long long dp[maxv];
 int main()
@@ -10,18 +10,15 @@ int main()
     int n,v;
     while(cin >> n >> v)
     {
+        fill(w,w+maxn,0);
         for(int i = 0;i < n;i++)
             cin >> w[i];
         fill(dp,dp+maxn,0);
         dp[0] = 1;
         sort(w,w + n);
         for(int i = 0;i < n;i++)
-        {
             for(int j = w[i];j <= v;j++)
-            {
                 dp[j] = dp[j] + dp[j - w[i]];
-            }
-        }
         cout << dp[v] << endl;
     }
     return 0;
